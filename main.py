c42379f6cad2725b34e45d729979e1871ad23914
Jakub Łągwa <kklagwa@gmail.com>
2023-01-17 12:39:30 +0100

    adding code

--- main.py
+++ main.py
@@ -1,16 +1,120 @@
-# This is a sample Python script.
+import turtle
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(800, 600)
+wn.tracer(0)
 
+#Score
+score_a = 0
+score_b = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(5, 1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(5, 1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.got(0, 0)
+ball.dx = 2
+ball.dy = 2
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B:0", align="center", font=("Courier", 24, "normal"))
+
+#Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+#Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+#Main game loop
+while True:
+    wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+
+    #Top and bottom
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        os.system("afplay bounce.wav&")
+
+    elif ball.ycor() > -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        os.sytem("afplay bounce.wav&")
+
+    #Left and right
+    if ball.xcor() > 350:
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a
+    , score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+        #Paddle and ball collisions
+        if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and
+            ball.ycor() > paddle_a.ycor() - 50:
+            ball.dx *= -1
+            os.system("afplay bounce.wav&")
+
+        elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and
+            ball.ycor() > paddle_a.ycor() - 50:
+            ball.dx *= -1
+            os.system("afplay bounce.wav&")
+
